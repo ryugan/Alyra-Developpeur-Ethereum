d7d9b36c3cbc54e7f144ca6ecd4685c5107bd6ad
Fabien <fabien.deliancourt@gmail.com>
2022-11-13 18:26:19 +0100

    Ajout de la configuration pour goerli

--- 3. Dapp/hardhat.config.ts
+++ 3. Dapp/hardhat.config.ts
@@ -6,7 +6,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + '/.env'});
 
 const defaultEthBalance: string = ethers.utils.parseEther('1000').toString();
-const accounts: HardhatNetworkAccountUserConfig[] = [
+const defaultHardhatAccounts: HardhatNetworkAccountUserConfig[] = [
   {privateKey:'0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', balance:defaultEthBalance},
   {privateKey:'0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', balance:defaultEthBalance},
   {privateKey:'0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a', balance:defaultEthBalance},
@@ -14,16 +14,26 @@ const accounts: HardhatNetworkAccountUserConfig[] = [
   {privateKey:'0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a', balance:defaultEthBalance},
 ];
 
-if (process.env.PRIVATE_KEY != undefined) {
-  accounts.unshift({privateKey:`${process.env.PRIVATE_KEY}`, balance:defaultEthBalance});
+const INFURA_KEY: string = process.env.INFURA_KEY ?? '';
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? '';
+
+if (PRIVATE_KEY!= undefined) {
+  defaultHardhatAccounts.unshift({privateKey:`${process.env.PRIVATE_KEY}`, balance:defaultEthBalance});
 }
+
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 1337,
-      accounts: accounts
+      accounts: defaultHardhatAccounts
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+      accounts: [PRIVATE_KEY]
     }
   },
   typechain: {
